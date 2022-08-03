369294766e4bf0382693e246ab70e48a13e727a4
Anatolii Krivchenko <akrivchenko@outlook.com>
2022-08-03 21:11:55 +0200

    Refactored implementation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cat-blender
+
+A program to merge/blend two cat pictures. Result is saved to `cat-card.jpg` file.
+
+## Prerequisites
+
+- Node >= 14.\*
+
+## Usage
+
+1. `npm install`
+2. `node . --greeting=hey --color=red` (example)
+
+## Parameters
+
+- greeting
+- who
+- width
+- height
+- color
+- size
---
+++ index.js
@@ -0,0 +1,20 @@
+import mergeImg from 'merge-img';
+import minimist from 'minimist';
+import fetch from 'node-fetch'; // TODO: can be removed when Fetch Api is stable in node 18
+
+const argv = minimist(process.argv.slice(2));
+const { greeting = 'Hello', who = 'You', width = 400, height = 500, color = 'Pink', size = 100 } = argv;
+
+async function fetchImage(keyword) {
+  const response = await fetch(
+    `https://cataas.com/cat/says/${keyword}?width=${width}&height=${height}&color=${color}&s=${size}`
+  );
+
+  console.log('Received response with status: ', response.status);
+  return Buffer.from(await response.arrayBuffer(), 'binary');
+}
+
+const first = await fetchImage(greeting);
+const second = await fetchImage(who);
+const result = await mergeImg([first, second]);
+result.write('cat-card.jpg', () => console.log('The file was saved!'));
